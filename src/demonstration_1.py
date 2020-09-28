9e72364dae8ea85fae30976eb180cb8a503617f5
Brian Vilchez <bfvilchez@icloud.com>
2020-09-28 11:12:38 -0600

    finished demostration_1

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -19,7 +19,29 @@ The assumptions we are making about our input are the following:
 """
 def split_in_parts(s, part_length):
     # Your code here
+    # split input string into smaller chuncks of the specified size.
+    # iterate over characters in the input string. 
+    # counter that will count up to part_length characters.
+    
 
+    output = []
+    counter = 0
 
+    for letter in s:
+        if counter == part_length:
+            output.append(" ")
+            output.append(letter)
+            counter = 1
+        else:
+            output.append(letter)
+            counter += 1
+
+    print(output)
+    return output
 # Your code here
+
+
+random_word = "randomStringAgain"
+
+split_in_parts(random_word, 4)
 
